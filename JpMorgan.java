68271314fdf3c5473158e0034ba11c93c29a0607
madswta <165416584+madswta@users.noreply.github.com>
2024-03-29 19:39:50 +0000

    Update JpMorgan.java

--- JpMorgan.java
+++ JpMorgan.java
@@ -14,25 +14,28 @@ public class JpMorgan {
      * Method for displaying word occurrences from a text file using serial stream
      * JP Morgan interview question
      * Chose to read all lines at once to reduce number of IO operations
+     * Takes 94 ms for processing
      */
     public static void main(String[] args) {
         long timeStart = System.currentTimeMillis();
         final String filePath = "/Volumes/SwatiWorkspace/javapractice/Exercise2/src/main/resources/JpMorgan.txt";
-        StringBuilder content = new StringBuilder();
+        StringBuilder fileContent = new StringBuilder();
         HashMap<String, Integer> wordMap = new HashMap<>();
         try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
-            stream.forEach(s -> content.append(s).append("\n"));
+            stream.forEach(s -> fileContent.append(s).append("\n"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String[] words = content.toString().replace(".\n", " ").replace(".","").replace(",", "").split(" ");
+
+        //Code for word occurrences count
+        String[] words = fileContent.toString().replace(".\n", " ").replace(".","").replace(",", "").split(" ");
         Arrays.stream(words).forEach(word -> {
-            String wd = word.trim();
-            int count = wordMap.getOrDefault(wd.trim(), 0);
+            String wd = word.trim().toLowerCase();
+            int count = wordMap.getOrDefault(wd, 0);
             wordMap.put(wd, count + 1);
         });
         long timeEnd = System.currentTimeMillis();
-        System.out.println(wordMap);
-        System.out.println(timeEnd - timeStart); //Time taken by serial stream
+        System.out.println("Displaying Word Occurrences" + wordMap);
+        System.out.println("time taken" + (timeEnd - timeStart)); //Time taken by serial stream
     }
 }
