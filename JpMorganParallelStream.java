f760a1fff03312ef10f600cbe3526daeb6598880
madswta <165416584+madswta@users.noreply.github.com>
2024-03-29 19:40:55 +0000

    Update JpMorganParallelStream.java

--- JpMorganParallelStream.java
+++ JpMorganParallelStream.java
@@ -1,41 +1,47 @@
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.stream.Stream;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.LongAdder;
 
 /**
  * @Author Swati Madaan
  */
 public class JpMorganParallelStream {
-        /**
-         * @param args
-         * Method for displaying word occurrences from a text file using parallel stream
-         * JP Morgan interview question
-         * Chose to read all lines at once to reduce number of IO operations
-         */
-        public static void main(String[] args) {
-            long timeStart = System.currentTimeMillis();
-            final String filePath = "/Volumes/SwatiWorkspace/javapractice/Exercise2/src/main/resources/JpMorgan.txt";
-            StringBuilder content = new StringBuilder();
-            HashMap<String, Integer> wordMap = new HashMap<>();
-            try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
-                stream.forEach(s -> content.append(s).append("\n"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            String[] words = content.toString().replace(".\n", " ").replace(".","").replace(",", "").split(" ");
-            Arrays.stream(words).parallel().forEach(word -> {
-                String wd = word.trim();
-                int count = wordMap.getOrDefault(wd.trim(), 0);
-                wordMap.put(wd, count + 1);
-            });
-            long timeEnd = System.currentTimeMillis();
-            System.out.println(wordMap);
-            System.out.println(timeEnd - timeStart); //Time taken by parallel stream
+    /**
+     * @param args Method for displaying word occurrences from a text file using parallel stream / multi threaded environment
+     *             JP Morgan interview question
+     *             Chose to read all lines at once to reduce number of IO operations
+     *             Took 98 ms for processing sample file.
+     *             Took a bit more time due to synchronization. Synchronization is required for removing race condition of concurrent hash map
+     *             and giving accurate result
+     */
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        ConcurrentHashMap<String, Integer> wordOccurrences = new ConcurrentHashMap<>();
+        final String filePath = "/Volumes/SwatiWorkspace/javapractice/Exercise2/src/main/resources/JpMorgan.txt";
+        Path file = Paths.get(filePath);
+
+        //Code for reading lines and counting words
+        try {
+            Files.readAllLines(file).parallelStream()
+                    .map(line -> line.replace("\n", " ").replace(".", "").replace(",", "").split(" ")).flatMap(Arrays::stream).parallel()
+                    .forEach(word -> {
+                        String wd = word.trim().toLowerCase();
+                        synchronized(wordOccurrences) {
+                            Integer ctr = (wordOccurrences.get(wd) == null) ? 0 :wordOccurrences.get(wd);
+                            ctr++;
+                            wordOccurrences.put(wd, ctr);
+                        }
+                    });
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        long end = System.currentTimeMillis();
+        System.out.println("Displaying word occurrences" + wordOccurrences);
+        System.out.println("Time taken" + (end - start));
     }
+}
 
 
